67da0a568b06b3ac24b86644a9412560c422e078
Amrit Adhikari <117933278+Amrit-Adhikary@users.noreply.github.com>
2024-11-12 11:26:58 +0545

    Update views.py

--- Django Application/ml_app/views.py
+++ Django Application/ml_app/views.py
@@ -323,6 +323,39 @@ def predict_page(request):
             print(f"Exception occurred during prediction: {e}")
             return render(request, 'cuda_full.html')
 
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegistrationForm, UserLoginForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('ml_app:index') 
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('ml_app:login')  
+
 def about(request):
     return render(request, ABOUT_TEMPLATE)
 
