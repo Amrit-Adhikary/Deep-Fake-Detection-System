f70bd71cde8e6e8f6e697d9e37b075318af41a79
Amrit Adhikari <117933278+Amrit-Adhikary@users.noreply.github.com>
2024-11-12 11:25:16 +0545

    Update urls.py

--- Django Application/ml_app/urls.py
+++ Django Application/ml_app/urls.py
@@ -3,13 +3,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import about, index, predict_page,cuda_full
+from .views import user_login, user_logout, register, about, index, predict_page, cuda_full
 
 app_name = 'ml_app'
 handler404 = views.handler404
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('home/', index, name='index'),
+    path('', user_login ,name="login"),
+    path('logout', user_logout,name="logout"),
+    path('register/', register ,name="register"),
     path('about/', about, name='about'),
     path('predict/', predict_page, name='predict'),
     path('cuda_full/',cuda_full,name='cuda_full'),
